a93904bd62a59390c3da6fad70b915f5e66148e7
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-10-21 19:38:31 -0300

    finalizado requisito10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
-    const missoes = 'Missões';
+    const missoesTitle = 'Missões';
     return (
       <div data-testid="missions">
-        <Title headline={ missoes } />
+        <Title headline={ missoesTitle } />
+        {
+          missions.map(({ name, year, country, destination }) => (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+
+            />
+          ))
+        }
       </div>
     );
   }
